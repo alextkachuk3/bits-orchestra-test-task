f12a49094bccc0d08c3c48a0c460176adb266665
Oleksii Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2024-09-21 17:54:41 +0300

    CSV file upload (test print to console)

--- bits-orchestra-test-task/Controllers/HomeController.cs
+++ bits-orchestra-test-task/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using bits_orchestra_test_task.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text;
 
 namespace bits_orchestra_test_task.Controllers
 {
@@ -16,7 +18,44 @@ namespace bits_orchestra_test_task.Controllers
         public IActionResult Index()
         {
             return View();
-        }        
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded");
+            }
+
+            using (var reader = new StreamReader(file.OpenReadStream(), Encoding.UTF8))
+            {
+                bool skipHeader = true;
+
+                while (!reader.EndOfStream)
+                {
+                    var line = await reader.ReadLineAsync();
+
+                    if (skipHeader)
+                    {
+                        skipHeader = false;
+                        continue;
+                    }
+
+                    var values = line.Split(',');
+
+                    var name = values[0];
+                    var dob = DateTime.Parse(values[1], CultureInfo.InvariantCulture);
+                    var married = bool.Parse(values[2]);
+                    var phone = values[3];
+                    var salary = decimal.Parse(values[4]);
+
+                    Console.WriteLine($"Name: {name}, Date of Birth: {dob.ToShortDateString()}, Married: {married}, Phone: {phone}, Salary: {salary}");
+                }
+            }
+
+            return Redirect("/");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
