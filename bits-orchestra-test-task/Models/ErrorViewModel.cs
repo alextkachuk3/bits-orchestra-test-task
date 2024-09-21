fa0891f810e4b3b9d4feca88d56489d47383cefc
Oleksii Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2024-09-21 16:24:42 +0300

    Add project files.

---
+++ bits-orchestra-test-task/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bits_orchestra_test_task.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
