a39714dfab37ef6405bf606369c43fc11a6496c1
Oleksii Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2024-09-21 18:12:48 +0300

    Employee model class

---
+++ bits-orchestra-test-task/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace bits_orchestra_test_task.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsMarried { get; set; }
+        public required string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+}
